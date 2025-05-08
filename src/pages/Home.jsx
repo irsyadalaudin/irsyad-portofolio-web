8d69c370790db111d8223603fe3a79959700f42b
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-05-08 21:17:32 +0700

    feat: create responsive layout in Home.jsx with 2/3 right and 1/3 left chore: add @tailwind utilities and remove unnecessary text-align in App.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 const Home = () => {
     return (
-        <>
-            <h1>Hi I'm Irsyad Alaudin Hariono</h1>
-            <h3>I'm Fullstack Javascript MERN developer</h3>
-            <p>I'm passionate Frontend & FUllstack Developer with a strong foundation in building modern web application. I specialize in using technologies like React js, Node.js, and MongoDB to create dynamic and responsive websites. With a keen eye for design and a focus on user experience, I strive to deliver clean, maintainable code that scalse with our project's needs. Let's build something amazing together!</p>
-        </>
+        <div className='flex'>
+            <div className='w-2/3'>
+                <h1>Hi I'm Irsyad Alaudin Hariono</h1>
+                <h3>I'm Fullstack Javascript MERN developer</h3>
+                <p>I'm passionate Frontend & FUllstack Developer with a strong foundation in building modern web application. I specialize in using technologies like React js, Node.js, and MongoDB to create dynamic and responsive websites. With a keen eye for design and a focus on user experience, I strive to deliver clean, maintainable code that scalse with our project's needs. Let's build something amazing together!</p>
+            </div>
+            <div className='w-1/3'>
+                <img src='' alt='photos' />
+            </div>
+        </div>
     )
 }
 
