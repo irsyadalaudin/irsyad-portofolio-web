dc0da416490bc4ce4bd5aba9ce9dda4258e7cab9
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-05-08 21:01:05 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <>
+            <h1>Hi I'm Irsyad Alaudin Hariono</h1>
+            <h3>I'm Fullstack Javascript MERN developer</h3>
+            <p>I'm passionate Frontend & FUllstack Developer with a strong foundation in building modern web application. I specialize in using technologies like React js, Node.js, and MongoDB to create dynamic and responsive websites. With a keen eye for design and a focus on user experience, I strive to deliver clean, maintainable code that scalse with our project's needs. Let's build something amazing together!</p>
+        </>
+    )
+}
+
+export default Home
