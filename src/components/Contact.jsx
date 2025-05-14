a66f67ff5e82c29e93cb0b4cc77032d0cc211eb3
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-05-14 12:53:25 +0700

    feat: create Contact.jsx Pages and import it in App.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+const Contact = () => {
+    return (
+        <>
+            <h1>CONTACT</h1>
+            <ul>
+                <li>Email: <a href='irsyadalaudin333@gmail.com'></a></li>
+                <li>Instagram: <a href='irsyadaldhr'></a></li>
+                <li>Jobstreet: <a href='jobstreet'></a></li>
+                <li>Glints: <a href='glints'></a></li>
+                <li>Github: <a href='github'></a></li>
+                <li>Gitlab: <a href='gitlab'></a></li>
+            </ul>
+        </>
+    )
+}
+
+export default Contact
