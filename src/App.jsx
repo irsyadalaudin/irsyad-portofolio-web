3496df753c06249af9c48135dc22ae1a4ffcd13b
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-05-14 11:27:50 +0700

    feat: create Home.jsx Pages and import it in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
+import Home from './pages/Home'
 
 const App = () => {
 	return (
 		<>
-			<h1 className='text-3xl font-bold underline bg-blue-500'>Hello world!</h1>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, deleniti odit officiis quas beatae dolorum tenetur? Eveniet mollitia impedit repudiandae ab, explicabo ducimus corrupti porro neque iste! Dicta, nesciunt vitae.</p>
+			<Home />
 		</>
 	)
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+    return (
+            <div className='flex h-90'>
+                <div className='w-2/3'>
+                    <h1>Hi I'm Irsyad Alaudin Hariono</h1>
+                    <h3>I'm Fullstack Javascript MERN developer</h3>
+                    <p>I'm passionate Frontend & FUllstack Developer with a strong foundation in building modern web application. I specialize in using technologies like React js, Node.js, and MongoDB to create dynamic and responsive websites. With a keen eye for design and a focus on user experience, I strive to deliver clean, maintainable code that scalse with our project's needs. Let's build something amazing together!</p>
+                </div>
+                <div className='w-1/3'>
+                    <img src='abc' alt='photos' />
+                </div>
+            </div>
+    )
+}
+
+export default Home
