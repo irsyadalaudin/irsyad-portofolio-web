61c6759e5b6ba5fcc618d3004fe11afda55f6f81
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-05-14 10:00:25 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+const App = () => {
+	return (
+		<>
+			<h1 className='text-3xl font-bold underline bg-blue-500'>Hello world!</h1>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, deleniti odit officiis quas beatae dolorum tenetur? Eveniet mollitia impedit repudiandae ab, explicabo ducimus corrupti porro neque iste! Dicta, nesciunt vitae.</p>
+		</>
+	)
+}
+
+export default App
