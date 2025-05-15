d2bac4818c3be2065b584ace50ebf37225109c2c
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-05-15 10:50:38 +0700

    chore: update styles and inner text content in AboutMe.jsx - update usage of `h-100` class in AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,11 +1,18 @@
 const AboutMe = () => {
     return (
-        <div className='flex items-center h-90'>
-            <div className='flex-col'>
-                <h1>ABOUT ME</h1>
-                <p>I graduated from Al-Azhar University in Cairo, majoring in Islamic Studies. Despite my non-technica; background, Idiscovered my upassion for web development and pursued it seriously. I started learning through online courses at Dicoding and completed an intensive 5-month fullstack javascript bootcamp at GOMYCODE Egypt. Since then, I have been consistently building projects using the MERN stack and continuously improving my skills through hands-on practice and real-world challenges</p>
+        <section id='aboutMe' className='h-100 px-8 flex items-center text-gray-800'>
+            <div className='flex-col max-w-4xl mx-auto space-y-6'>
+                <h1 className='text-3xl font-bold border-b-2 border-indigo-500 inline-block pb-2'>
+                    ABOUT ME
+                </h1>
+                <p className='text-lg leading-relaxed'>
+                    I graduated from Al-Azhar University in Cairo, majoring in Islamic Studies. Despite my non-technical background, I discovered my passion for web development and pursue it seriously. 
+                </p>
+                <p className='text-lg leading-relaxed'>
+                    I started learning through online courses at Dicoding and completed an intensive 5-month fullstack javascript bootcamp at GOMYCODE Egypt. Since then, I have been building real world projects using the MERN stack and continuously improving my skills through hands-on practice and constantly sharpening my skills
+                </p>
             </div>
-        </div>
+        </section>
     )
 }
 
