7f5c1cc249ab109d5d4bc9802db8d2ffe895a070
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-05-17 23:10:46 +0700

    chore: update styling for layout and responsiveness in Projects.jsx - remove `bg-white` and `text-gray-800` classes - change `max-w-4xl` to `max-w-5xl` - update `px-12` to `px-16` on element with `key={index}`

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -22,14 +22,14 @@ const Projects = () => {
 
 
     return (
-        <section id='projects' className='h-100 flex items-center bg-white text-gray-800'>
-            <div className='max-w-4xl mx-auto space-y-6'>
+        <section id='projects' className='h-100 flex items-center'>
+            <div className='max-w-5xl mx-auto space-y-6'>
                 <h1 className='pb-2 text-3xl font-bold border-b-2 border-indigo-500 inline-block'>
                     PROJECTS
                 </h1>
                 <div className='grid md:grid-cols-3 gap-6 mt-8'>
                     {projectList.map((project, index) => (
-                        <div key={index} className='border rounded-xl py-6 px-12 shadow hover:shadow-lg transition'>
+                        <div key={index} className='border rounded-xl py-6 px-16 shadow hover:shadow-lg transition'>
                             <img src={project.image} alt='image' className='border-indigo-500' />
                             <h3 className='text-xl font-semibold mb-2'>{project.title}</h3>
                             <div className='flex gap-4'>
@@ -44,32 +44,6 @@ const Projects = () => {
                     ))}
                 </div>
             </div>
-            {/* <h3>Here are some of the projects I have worked on:</h3>
-            <div>
-                <h4>MERN WAREHOUSE</h4>
-                <link rel='stylesheet' href='netlify' />
-                <link rel='stylesheet' href='github' />
-            </div>
-            <div>
-                <h4>MERN RECIPE FINDER</h4>
-                <link rel='stylesheet' href='netlify' />
-                <link rel='stylesheet' href='github' />
-            </div>
-            <div>
-                <h4>REACT REDUX</h4>
-                <link rel='stylesheet' href='netlify' />
-                <link rel='stylesheet' href='github' />
-            </div>
-            <div>
-                <h4>REACT ROUTER</h4>
-                <link rel='stylesheet' href='netlify' />
-                <link rel='stylesheet' href='github' />
-            </div>
-            <div>
-                <h4>MONGO DB</h4>
-                <link rel='stylesheet' href='netlify' />
-                <link rel='stylesheet' href='github' />
-            </div> */}
         </section>
     )
 }
