55632ac49858f27d3b17fd947340a8f49e2a33e1
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-05-18 11:03:47 +0700

    chore: remove unused `Projects` import from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import AboutMe from './components/AboutMe'
-import Projects from './components/Projects'
 import TechStack from './components/TechStack'
 import Home from './pages/Home'
 
