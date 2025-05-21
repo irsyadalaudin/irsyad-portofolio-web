bc4d8d3eafa0356abe00b794c9ff46e2bf1f084e
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-05-21 23:50:59 +0700

    chore: replace wrapper section with <div> and restructure ABOUT ME and EXPERIENCE sections in AboutMe.jsx
    - move Tailwind classes to the wrapper <div>

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -27,9 +27,9 @@ const AboutMe = () => {
     ]
 
     return (
-        <section id='aboutMe' className='py-16 text-gray-800'>
-            <div className='max-w-5xl mx-auto space-y-12'>
-                <div className='space-y-6'>
+        <div id='aboutMe' className='py-16 text-gray-800 max-w-5xl mx-auto space-y-12'>
+            {/* <div className='max-w-5xl mx-auto space-y-12'> */}
+                <section id='aboutMe' className='space-y-6'>
                     <h1 className='text-3xl font-bold border-b-2 border-indigo-500 inline-block pb-2'>
                         ABOUT ME
                     </h1>
@@ -39,9 +39,8 @@ const AboutMe = () => {
                     <p className='text-lg leading-relaxed'>
                         I started learning through online courses at Dicoding and completed an intensive 4-month fullstack javascript bootcamp at GOMYCODE, Cairo, Egypt. Since then, I have been building real world projects using the MERN stack and continuously improving my skills through hands-on practice and constantly sharpening my skills
                     </p>
-                </div>
-
-                <div className='space-y-6'>
+                </section>
+                <section id='experience' className='space-y-6'>
                     <h1 className='text-3xl font-bold border-b-2 border-indigo-500 inline-block pb-2'>
                         EXPERIENCE
                     </h1>
@@ -55,9 +54,9 @@ const AboutMe = () => {
                             </div>
                         ))}
                     </div>
-                </div>
-            </div>
-        </section>
+                </section>
+            {/* </div> */}
+        </div>
     )
 }
 
