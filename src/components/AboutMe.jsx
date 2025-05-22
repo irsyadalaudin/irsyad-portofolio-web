369a31224e3aaa04ec0e7ba083b12036a1d15ec7
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-05-22 14:57:12 +0700

    chore: move `max-w-5xl mx-auto` classes to ABOUT ME and EXPERIENCE <section> elements in AboutMe.jsx - comment out unnecessary <div> wrapper to compare styling with <section>

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -27,33 +27,34 @@ const AboutMe = () => {
     ]
 
     return (
-        <div id='aboutMe' className='py-16 text-gray-800 max-w-5xl mx-auto space-y-12'>
-                <section id='aboutMe' className='space-y-6'>
-                    <h1 className='text-3xl font-bold border-b-2 border-indigo-500 inline-block pb-2'>
-                        ABOUT ME
-                    </h1>
-                    <p className='text-lg leading-relaxed'>
-                        I graduated from Al-Azhar University in Cairo, majoring in Islamic Studies. Despite my non-technical background, I discovered my passion for web development and pursue it seriously. 
-                    </p>
-                    <p className='text-lg leading-relaxed'>
-                        I started learning through online courses at Dicoding and completed an intensive 4-month fullstack javascript bootcamp at GOMYCODE, Cairo, Egypt. Since then, I have been building real world projects using the MERN stack and continuously improving my skills through hands-on practice and constantly sharpening my skills
-                    </p>
-                </section>
-                <section id='experience' className='space-y-6'>
-                    <h1 className='text-3xl font-bold border-b-2 border-indigo-500 inline-block pb-2'>
-                        EXPERIENCE
-                    </h1>
-                    <div className='grid gap-6'>
-                        {experienceList.map((exp, index) => (
-                            <div key={index} className='bg-gray-50 p-6 rounded-l shadow hover:shadow-md transition duration-200 border-l-4 border-indigo-500'>
-                                <p className='text-sm text-gray-500'>{exp.year}</p>
-                                <h2 className='text-xl font-semibold'>{exp.role}</h2>
-                                <p className='text-gray-600 italic'>{exp.place}</p>
-                                <p className='mt-2 text-gray-700'>{exp.description}</p>
-                            </div>
-                        ))}
-                    </div>
-                </section>
+        // <div id='aboutMe' className='py-16 px-8 text-gray-800 max-w-5xl mx-auto space-y-12'>
+        <div id='aboutMe' className='py-16 px-8 text-gray-800 space-y-12'>
+            <section id='aboutMe' className='max-w-5xl mx-auto space-y-6'>
+                <h1 className='text-3xl font-bold border-b-2 border-indigo-500 inline-block pb-2'>
+                    ABOUT ME
+                </h1>
+                <p className='text-lg leading-relaxed'>
+                    I graduated from Al-Azhar University in Cairo, majoring in Islamic Studies. Despite my non-technical background, I discovered my passion for web development and pursue it seriously. 
+                </p>
+                <p className='text-lg leading-relaxed'>
+                    I started learning through online courses at Dicoding and completed an intensive 4-month fullstack javascript bootcamp at GOMYCODE, Cairo, Egypt. Since then, I have been building real world projects using the MERN stack and continuously improving my skills through hands-on practice and constantly sharpening my skills
+                </p>
+            </section>
+            <section id='experience' className='max-w-5xl mx-auto space-y-6'>
+                <h1 className='text-3xl font-bold border-b-2 border-indigo-500 inline-block pb-2'>
+                    EXPERIENCE
+                </h1>
+                <div className='grid gap-6'>
+                    {experienceList.map((exp, index) => (
+                        <div key={index} className='bg-gray-50 p-6 rounded-l shadow hover:shadow-md transition duration-200 border-l-4 border-indigo-500'>
+                            <p className='text-sm text-gray-500'>{exp.year}</p>
+                            <h2 className='text-xl font-semibold'>{exp.role}</h2>
+                            <p className='text-gray-600 italic'>{exp.place}</p>
+                            <p className='mt-2 text-gray-700'>{exp.description}</p>
+                        </div>
+                    ))}
+                </div>
+            </section>
         </div>
     )
 }
