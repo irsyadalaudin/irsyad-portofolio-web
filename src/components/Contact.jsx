d0bd4593b112eb5b6fa26a6bc6b9b055d2e31734
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-05-24 05:56:23 +0700

    chore: replace `max-w-5xl` with `px-8` classes for layout consistency in <section> element in Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 const Contact = () => {
     return (
-        <section id='contact' className='py-16 text-gray-800 max-w-5xl mx-auto space-y-6'>
+        <section id='contact' className='py-20 px-8 text-gray-800 mx-auto space-y-6'>
             <div className='max-w-5xl mx-auto space-y-6'>
                 <h1 className='text-3xl font-bold border-b-2 border-indigo-500 inline-block pb-2'>CONTACT ME</h1>
                 <h3 className='text-xl'>Feel free to reach out via email or connect with me on social platforms:</h3>
