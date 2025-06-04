6d3fd35740e2c323b74152a3d03a112ac72507ed
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-06-04 23:24:17 +0700

    feat: add Tailwind dark mode classes in TechOverview.jsx

--- src/components/TechOverview.jsx
+++ src/components/TechOverview.jsx
@@ -25,52 +25,51 @@ const TechOverview = () => {
 
     return (
         <div className='h-screen px-8 flex flex-col justify-center space-y-12'>
-            <section id='techStack' className='text-gray-800 max-w-5xl mx-auto space-y-6 scroll-mt-28'>
+            <section id='techStack' className='text-gray-800 dark:text-indigo-100 max-w-5xl mx-auto space-y-6 scroll-mt-28'>
                 <h1 className='pb-2 text-3xl font-bold border-b-2 border-indigo-500 inline-block'>
                     MY TECH OVERVIEW
                 </h1>
                 <div className='grid grid-cols-2 md:grid-cols-3 gap-6 mt-8 text-center'>
-                    <div className='py-4 px-6 bg-white shadow rounded-xl'>
+                    <div className='py-4 px-6 rounded-xl bg-white dark:bg-gray-900 shadow hover:shadow-lg dark:shadow-gray-950'>
                         <SiJavascript className='text-4xl text-yellow-400 mx-auto' />
-                        <p className='mt-2 font-semibold'>Javascript</p>
+                        <p className='mt-2 font-semibold text-gray-800 dark:text-white'>Javascript</p>
                     </div>
-                    <div className='py-4 px-6 bg-white shadow rounded-xl'>
+                    <div className='py-4 px-6 rounded-xl bg-white dark:bg-gray-900 shadow hover:shadow-lg dark:shadow-gray-950'>
                         <FaReact className='text-4xl text-blue-500 mx-auto' />
-                        <p className='mt-2 font-semibold'>React.js</p>
+                        <p className='mt-2 font-semibold text-gray-800 dark:text-white'>React.js</p>
                     </div>
-                    <div className='py-4 px-6 bg-white shadow rounded-xl'>
+                    <div className='py-4 px-6 rounded-xl bg-white dark:bg-gray-900 shadow hover:shadow-lg dark:shadow-gray-950'>
                         <FaNodeJs className='text-4xl text-green-600 mx-auto' />
-                        <p className='mt-2 font-semibold'>Node.js</p>
+                        <p className='mt-2 font-semibold text-gray-800 dark:text-white'>Node.js</p>
                     </div>
-                    <div className='py-4 px-6 bg-white shadow rounded-xl'>
+                    <div className='py-4 px-6 rounded-xl bg-white dark:bg-gray-900 shadow hover:shadow-lg dark:shadow-gray-950'>
                         <FaDatabase className='text-4xl text-yellow-600 mx-auto' />
-                        <p className='mt-2 font-semibold'>MongoDB</p>
+                        <p className='mt-2 font-semibold text-gray-800 dark:text-white'>MongoDB</p>
                     </div>
-                    <div className='py-4 px-6 bg-white shadow rounded-xl'>
+                    <div className='py-4 px-6 rounded-xl bg-white dark:bg-gray-900 shadow hover:shadow-lg dark:shadow-gray-950'>
                         <FaGitAlt className='text-4xl text-orange-600 mx-auto' />
-                        <p className='mt-2 font-semibold'>Git & GitHub</p>
+                        <p className='mt-2 font-semibold text-gray-800 dark:text-white'>Git & GitHub</p>
                     </div>
-                    <div className='py-4 px-6 bg-white shadow rounded-xl'>
-                        <p className='text-2xl font-bold'>+</p>
-                        <p className='mt-2 break-words'>Express.js, REST API, Redux, React Router</p>
-                        <p></p>
+                    <div className='py-4 px-6 rounded-xl bg-white dark:bg-gray-900 shadow hover:shadow-lg dark:shadow-gray-950'>
+                        <p className='text-2xl font-bold text-gray-800 dark:text-white'>+</p>
+                        <p className='mt-2 font-semibolds break-words text-gray-800 dark:text-white'>Express.js, REST API, Redux, React Router</p>
                     </div>
                 </div>
             </section>
-            <section id='projects' className='text-gray-800 max-w-5xl mx-auto space-y-6'>
+            <section id='projects' className='text-gray-800 dark:text-indigo-100 max-w-5xl mx-auto space-y-6'>
                     <h1 className='pb-2 text-3xl font-bold border-b-2 border-indigo-500 inline-block'>
                         PROJECTS
                     </h1>
                     <div className='grid md:grid-cols-3 gap-6 mt-8'>
                         {projectList.map((project, index) => (
-                            <div key={index} className='border rounded-xl py-6 px-16 shadow hover:shadow-lg transition'>
+                            <div key={index} className='py-6 px-16 rounded-xl bg-white dark:bg-gray-900 shadow hover:shadow-lg dark:shadow-gray-950 transition'>
                                 <img src={project.image} alt='image' className='border-indigo-500' />
                                 <h3 className='text-xl font-semibold px-2 mb-2'>{project.title}</h3>
                                 <div className='flex gap-4'>
-                                    <a href={project.netlify} target='_blank' rel='noreferrer' className=' text-indigo-600 hover:underline'>
+                                    <a href={project.netlify} target='_blank' rel='noreferrer' className=' text-indigo-600 dark:text-indigo-400 hover:underline'>
                                         Live Demo
                                     </a>
-                                    <a href={project.github} target='_blank' rel='noreferrer' className='text-gray-600 hover:underline'>
+                                    <a href={project.github} target='_blank' rel='noreferrer' className='text-gray-600 dark:text-indigo-300 hover:underline'>
                                         GitHub
                                     </a>
                                 </div>
