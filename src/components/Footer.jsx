86c332161a784ca5fc9df7f02780df8bbdb34170
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-06-04 23:25:46 +0700

    feat: add Tailwind dark mode classes in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className='py-6 text-center bg-white text-gray-500 border-t'>
+        <footer className='py-6 text-center border-t-gray-950 bg-white dark:bg-gray-900 text-gray-500 dark:text-gray-300'>
             <p>© {new Date().getFullYear()} Irsyad Alaudin Hariono. All rights reserved.</p>
         </footer>
     )
